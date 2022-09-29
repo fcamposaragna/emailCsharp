9386593f1bd8c87846d4cc0792e60fb8d40dbaa3
Franco Camposaragna <fcamposaragna@gmail.com>
2022-09-29 15:19:04 +0100

    first commit

---
+++ SimpleEmailApp/Controllers/EmailController.cs
@@ -0,0 +1,32 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+
+namespace SimpleEmailApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult SendEmail(string body)
+        {
+            var email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse("sydnie.harber@ethereal.email"));
+            email.To.Add(MailboxAddress.Parse("sydnie.harber@ethereal.email"));
+            email.Subject = "Test mail";
+            email.Body = new TextPart(TextFormat.Html) { Text = body };
+
+            using var smpt = new SmtpClient();
+            smpt.Connect("smtp.ethereal.email", 587, SecureSocketOptions.StartTls);
+            smpt.Authenticate("sydnie.harber@ethereal.email", "FzqK9jtwww1CA14DtH");
+            smpt.Send(email);
+            smpt.Disconnect(true);
+
+            return Ok();
+        }
+    }
+}
